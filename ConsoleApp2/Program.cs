25898c77f4d0c8ca59b24c818e5b77c407088c73
Max <kizzytgg@gmail.com>
2021-06-24 19:11:43 +0300

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SP
+{
+    class Person
+    {
+        private string _A;
+        private string _B;
+
+        public Person(string S, string P)
+        {
+            _S = S;
+            _P = P;
+        }
+
+        public override string ToString()
+        {
+            return "The name of person is: " + _S + " The name of person is: " + _P;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new Person[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                string name = Console.ReadLine();
+                string surname = Console.ReadLine();
+                var person = new Person(name, surname);
+                people[i] = person;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(people[i].ToString());
+            }
+        }
+    }
+}
---
+++ Oop1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Oop1
+{
+    class SP
+    {
+        private string _name;
+        
+        // public string FirstName;
+        // public string LastName;
+
+        // public Person()
+        // {
+        //     Console.WriteLine("Default constructor called");
+        // }
+        
+        public (string name)
+        {
+            // Console.WriteLine("Parametrised constructor called");
+            _name = name;
+        }
+        
+        public override string ToString()
+        {
+            return "The name of person is: " + _name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new Person[3];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                string name = Console.ReadLine();
+                
+                // var person =  new Person();
+                // person.Name = name;
+                
+                var person =  new Person(name);
+                
+                people[i] = person;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(people[i].ToString());
+            } 
+        }
+    }
+}
